b0b3df9a13ab2b8979d15bdf6a49478f6c0665e0
lewie-jee <lewis.shriver@proton.me>
2026-06-01 14:43:43 +0200

    feat: implement Asteroid split fracturing logic and adaptive spawning
    - Add split method to Asteroid class to handle structural fracturing
    - Use random uniform angle distribution for realistic vector deflection
    - Scale fragment velocity dynamically to create faster-moving debris
    - Replace default kill sequence with split iteration inside the collision loop

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,7 @@
 from circleshape import CircleShape
-import pygame
-from constants import LINE_WIDTH
+import pygame, random
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x: float, y: float, radius: float) -> None:
@@ -11,3 +12,19 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += self.velocity * dt
+    
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        angle = random.uniform(20, 50)
+        new_vector_1 = self.velocity.rotate(angle)
+        new_vector_2 = self.velocity.rotate(-angle)
+        self.radius -= ASTEROID_MIN_RADIUS
+        new_asteroid_1 = Asteroid(self.position.x, self.position.y, self.radius)
+        new_asteroid_2 = Asteroid(self.position.x, self.position.y, self.radius)
+        new_asteroid_1.velocity = new_vector_1 * 1.2
+        new_asteroid_2.velocity = new_vector_2 * 1.2
+
+
