f8f2a8293e381d3f10a3555eeff01fa210fff992
lewie-jee <lewis.shriver@proton.me>
2026-06-01 12:48:27 +0200

    feat: implement Asteroid entities and automated Spawner system
    - Append asteroid dimension scaling and spawning metrics to constants.py
    - Create asteroid.py extending CircleShape with linear vector updating
    - Integrate asteroidfield.py to manage timed off-screen threat spawning
    - Hook Asteroid and AsteroidField static containers to Pygame groups in main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -17,6 +19,12 @@ def main():
     drawable = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    AsteroidField.containers = (updatable)
+    asteroidfield = AsteroidField()
 
     while True:
         log_state()
